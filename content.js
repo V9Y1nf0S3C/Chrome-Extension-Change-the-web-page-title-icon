2a40e682c638506ff49e17c2f7a51478e843d24c
Vinay Kumar <Vinay Kumar>
2023-01-19 00:17:21 +0800

    click to set manually + setTimeout

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
 // content.js
 //alert("Hello!")
+changeTheName();
+setTimeout(changeTheName, 5000);
+function changeTheName(){
 document.title = "My WebPage";
-var link = document.querySelector("link[rel~='icon']"); if (!link) { link = document.createElement('link'); link.rel = 'icon'; document.head.appendChild(link); } link.href = 'https://stackoverflow.com/favicon.ico';
+var link = document.querySelector("link[rel~='icon']"); if (!link) { link = document.createElement('link'); link.rel = 'icon'; document.head.appendChild(link); } link.href = 'https://stackoverflow.com/favicon.ico';}
--- content2.js
+++ content2.js
@@ -1,4 +1,7 @@
 // content.js
 //alert("Hello!")
-document.title = "Docs";
-var link = document.querySelector("link[rel~='icon']"); if (!link) { link = document.createElement('link'); link.rel = 'icon'; document.head.appendChild(link); } link.href = 'https://github.com/favicon.ico';
+changeTheName();
+setTimeout(changeTheName, 2000);
+setTimeout(changeTheName, 5000);
+function changeTheName(){
+document.title = "Docs"; var link = document.querySelector("link[rel~='icon']"); if (!link) { link = document.createElement('link'); link.rel = 'icon'; document.head.appendChild(link); } link.href = 'https://github.com/favicon.ico'; }
